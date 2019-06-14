a6a0d5b3d94ebb96b9f89f77d9c26403270943e8
Anton <55433RAW@gmail.com>
2019-06-14 13:10:05 +0700

    Это наш первый комит

---
+++ script.js
@@ -0,0 +1,46 @@
+"use strict";
+
+/*var leftBorderWidth = 1;
+let second = 2;
+const pi = 3.14;
+
+second = 3;
+//console.log(second);
+
+let persone = {
+    name: "John",
+    age: 25,
+    isMarried: false
+};
+
+let arr = ['plum.png', 'orange.jper', 'apple.bpm'];
+console.log(arr[1]);
+
+/*alert("Hello world");
+let answer = confirm("Are you shure?");
+
+console.log(answer);
+*/
+//let answer = +prompt("Are you shure?", "Yes");
+
+//console.log(typeof(answer));
+
+//console.log("arr" + " - object");
+
+a = 5;
+console.log(a);
+
+let incr = 10,
+    decr = 10;
+
+
+    console.log(incr++);
+    console.log(decr--);
+
+    console.log(5%2);
+    console.log("2" === 2);
+
+    let isChecked = false,
+        isClose = false;
+
+        console.log(isChecked || !isClose);
